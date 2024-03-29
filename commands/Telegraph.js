012bc9e16391528e2c9250e24c2ecb58036d2423
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-29 23:52:04 +0100

    🎈

---
+++ commands/Telegraph.js
@@ -0,0 +1,26 @@
+/**
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : BAT-MD
+ * @author : Xcelsama <https://github.com/Xcelsama>
+ * @description : Secktor,A Multi-functional whatsapp bot.
+ * @version :-Latest
+ **/
+//---------------------------------------------------------------------------
+const { cmd }   = require('../lib');
+const util = require('util');
+const axios = require('axios');
+cmd({
+        pattern: "paste",
+        desc: "bat-bot creates paste of text on the bat-web.",
+        category: "tool🛠",
+        filename: __filename,
+    },
+    async(Void, citel,text) => {
+ let a = citel.quoted ? citel.quoted.text : citel.text;
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Secktor-Md+Bot&author_name=SamPandey001&content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
+    }
+);
